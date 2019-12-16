13d5c2978dd88be93062c7a06dfa97abe1588327
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-16 12:36:51 +0000

    update: BAS12 SliceSplice

--- BAS12-SliceSplice/variables.js
+++ BAS12-SliceSplice/variables.js
@@ -45,6 +45,17 @@ function frankenSplice(arr1, arr2, n) {
 }
 
 x = frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// Solution 2
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
 `;
 
 // === Desired Output (answer) here ===
