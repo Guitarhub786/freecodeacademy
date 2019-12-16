de0cf127f6bf7395e34b45e0479b3bcaf93c0be1
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-16 12:38:11 +0000

    update: BAS12 SliceSplice

--- BAS12-SliceSplice/variables.js
+++ BAS12-SliceSplice/variables.js
@@ -56,6 +56,12 @@ function frankenSplice(arr1, arr2, n) {
   }
   return localArray;
 }
+
+// Solution 3
+function frankenSplice(arr1, arr2, n) {
+
+  return [...arr2.slice(0,n),...arr1,...arr2.slice(n)];
+}
 `;
 
 // === Desired Output (answer) here ===
