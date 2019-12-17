816ff9ef61da12f6d36169a818a5f44854091848
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-17 13:41:51 +0000

    update: Finders Keepers

--- BAS9-FindersKeepers/variables.js
+++ BAS9-FindersKeepers/variables.js
@@ -47,14 +47,11 @@ function findElement(arr, func) {
 
 // Solution 4
 function findElement(arr, func) {
-
-  for (var i = 0; i < arr.length; i++) {
-
-    if (func(arr[i])) {
-      return arr[i];
-    }
+  
+  for (let i=0;i<arr.length;i++) {
+    if(func(arr[i])) return arr[i]; 
   }
-
+  
   return undefined;
 }
 `;
