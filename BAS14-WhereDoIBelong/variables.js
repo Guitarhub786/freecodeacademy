9bec6f03b97b05f7aac5343f21823c6fe6ff6f03
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-21 23:52:27 +0000

    update: BAS14 Where Do I Belong

--- BAS14-WhereDoIBelong/variables.js
+++ BAS14-WhereDoIBelong/variables.js
@@ -46,6 +46,19 @@ function getIndexToIns(arr, num) {
 }
 
 x = getIndexToIns([40, 60], 50);
+
+// Solution 2
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num) return a;
+  }
+
+  return arr.length;
+}
 `;
 
 // === Desired Output (answer) here ===
