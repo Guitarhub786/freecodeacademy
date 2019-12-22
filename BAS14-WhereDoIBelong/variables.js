be56897ec6a186181ae0f7f09283e659e95286b0
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-22 16:55:50 +0000

    update: BAS14 Where Do I Belong

--- BAS14-WhereDoIBelong/variables.js
+++ BAS14-WhereDoIBelong/variables.js
@@ -34,15 +34,17 @@ x = getIndexToIns([40, 60], 50);
 let displayAnswer =
   ` 
 function getIndexToIns(arr, num) {
-  // Find my place in this sorted array.
-  var times = arr.length; // runs the for loop once for each thing in the array
-  var count = 0;
-  for (var i = 0; i < times; i++) {
-    if (num > arr[i]) {
-      count++;
+
+  arr.sort(function(a,b) {
+    return a - b;
+  });
+
+  for (var i = 0; i < arr.length; i++) {
+    if (num <= arr[i]) {
+      return i;
     }
-  } // counts how many array numbers are smaller than num
-  return count; // the above equals num's position in a sorted array
+  } 
+  return arr.length; 
 }
 
 x = getIndexToIns([40, 60], 50);
