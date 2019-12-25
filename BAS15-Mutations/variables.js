34c75229b440e113676dcf42f37defbbedf38a77
Guitarhub786 <codewithdrawal@gmail.com>
2019-12-25 21:30:06 +0000

    update: mutations

--- BAS15-Mutations/variables.js
+++ BAS15-Mutations/variables.js
@@ -34,9 +34,9 @@ x = mutation(["Alien", "line"]);
 let displayAnswer =
   ` 
 function mutation(arr) {
-  var test = arr[1].toLowerCase();
-  var target = arr[0].toLowerCase();
-  for (var i = 0; i < test.length; i++) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
     if (target.indexOf(test[i]) < 0) return false;
   }
   return true;
